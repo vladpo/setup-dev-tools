0de8f96e8bdac25312c1d4cdf9f00109277372d7
Vlad Popovici <vlad.popovici@ullink.com>
2018-03-06 14:45:30 +0200

    add git install. define main

--- debian/oracle-java-8.sh
+++ debian/oracle-java-8.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo apt install -y software-properties-common
 sudo apt-add-repository -y ppa:webupd8team/java
 sudo apt update
 sudo echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
--- main.sh
+++ main.sh
@@ -1,3 +1,60 @@
 #!/bin/bash
 
-sudo apt install -y software-properties-common
+function substring() {
+  echo "*$1*"
+}
+
+function installGitRh() {
+  sudo yum update
+  sudo yum -y install git
+  OS="redhat"
+}
+
+function installGitDeb() {
+  sudo apt-get update
+  sudo apt-get install -y git
+  OS="debian"
+}
+
+if [[ ! -f $# ]]; then
+  echo "Usage: "
+  echo ""
+  echo "./main.sh /path-to-private-ssh-key-file"
+  exit 1
+fi
+
+CAT_CMD="sudo cat /etc/*-release | tr \"\\n\" ' '"
+RELEASE=`eval $CAT_CMD`
+RH=$(substring "Red Hat")
+CENTOS=$(substring "CentOS")
+DEB=$(substring "Debian")
+UBUNTU=$(substring "Ubuntu")
+OS=""
+
+cd /mnt
+
+case $RELEASE in
+  $RH) installGitRh;;
+  $CENTOS) installGitRh;;
+  $DEB) installGitDeb;;
+  $UBUNTU) installGitDeb;;
+  *) 
+    echo "Unknown Linux Distribution"
+    exit 1
+esac
+
+exit 0
+sudo git config --global user.email gaia.uat@ullink.com
+sudo git config --global user.name "Gaia Uat Ullink"
+sudo eval "$(ssh-agent -s)"
+sudo ssh-add $1
+
+sudo git clone git@gitlab.ullink.lan:information.systems/dev-tools-setup.git
+sudo chmod +x -R ./dev-tools-setup/
+cd dev-tools-setup/$OS
+
+for script in "$@"; do
+  sudo sh $script
+done
+
+echo "Finished"
